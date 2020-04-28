d8a1d2bf7a7d610437adbcaca4c817f201234b68
Andrew <katzensprung1@gmail.com>
2020-04-28 21:44:43 +1000

    david magic

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -4,14 +4,14 @@ const RESOURCES = {
   "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
 "assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
 "assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
-"assets/LICENSE": "0000d255865246c9b55862d96ffd3089",
+"assets/LICENSE": "52e454b4f08194546da8e2d3a09ae263",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "80136ea5fcca9457c13bdd1e6a685277",
 "/": "80136ea5fcca9457c13bdd1e6a685277",
-"main.dart.js": "748d654bd48d0bdac49c3eb5a7716569",
+"main.dart.js": "ecf2b17436993fa5e9dfa16d6ce67eb0",
 "manifest.json": "900bebb4eeddd7499c2671bcbfe218ac"
 };
 
